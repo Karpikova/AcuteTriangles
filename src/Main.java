b9487a97e30b70ce6521d7dd01084ee0bf6cc6f7
Maria Karpikova <makarpikova@gmail.com>
2017-08-10 15:19:41 +0300

    'goodd'

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 import java.util.Stack;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) { //this is master
         Handler handler = new Handler();
         System.out.printf(handler.getSum());
     }
