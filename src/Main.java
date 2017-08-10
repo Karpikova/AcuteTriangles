6ad483453c3ad75f700db80ea6500e78500639a4
Maria Karpikova <makarpikova@gmail.com>
2017-08-10 16:08:08 +0300

    'goodd'

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,7 @@
 import java.util.Stack;
 
 public class Main {
-<<<<<<< Updated upstream
-    public static void main(String[] args) { //trulala
-=======
-    public static void main(String[] args) { //this is withRM111
->>>>>>> Stashed changes
+    public static void main(String[] args) {
         Handler handler = new Handler();
         System.out.printf(handler.getSum());
     }
