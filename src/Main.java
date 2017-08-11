ddb01db16bf2d638ad7bab60f305af451c76248c
Karpikova <makarpikova@gmail.com>
2017-08-11 08:57:01 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 import java.util.Stack;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) { //I'm a stash!!!
         Handler handler = new Handler();
         System.out.printf(handler.getSum());
     }
