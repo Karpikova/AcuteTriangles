3db314b5775417da71b4ff6b47a4edbb6c698f00
Karpikova <makarpikova@gmail.com>
2017-08-11 10:32:08 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import java.util.Stack;
 
 public class Main {
     public static void main(String[] args) { //Lalala1
-        Handler handler = new Handler();
+        Handler handler = new Handler();    //trulala
         System.out.printf(handler.getSum());
     }
 
