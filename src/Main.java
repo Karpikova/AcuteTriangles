900a672811c39bc939f2b5769b64539b8594e4fe
Karpikova <makarpikova@gmail.com>
2017-08-11 10:25:47 +0300

    Update Main.java
    222

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 import java.util.Stack;
 
 public class Main {
-    public static void main(String[] args) { //Lalala
+    public static void main(String[] args) { //Lalala1
         Handler handler = new Handler();
         System.out.printf(handler.getSum());
     }
